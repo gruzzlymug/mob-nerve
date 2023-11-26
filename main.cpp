e61948222cb319b4f354dc62ca8fb96a6ef01820
gruzzlymug <kevin@gruzzlymug.com>
2023-11-25 22:50:57 -0600

    More rendering and key events

--- main.cpp
+++ main.cpp
@@ -20,16 +20,21 @@ int main() {
         exit(-1);
     }
 
+    IMG_Init(IMG_INIT_PNG);
+
     SDL_Window *window = SDL_CreateWindow("Mob Nerve A", 0, 0, SCREEN_HEIGHT, SCREEN_WIDTH, SDL_WINDOW_HIDDEN);
 
-    SDL_Surface* helloImage = IMG_Load("hello.png");
-    if (helloImage == NULL) {
-        printf("Unable to load image %s! SDL Error: %s\n", "hello.png", SDL_GetError());
+    SDL_Renderer* renderer = NULL;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Texture* helloTexture = IMG_LoadTexture(renderer, "hello.png");
+    if (helloTexture == NULL) {
+        printf("Unable to load texture %s! SDL Error: %s\n", "hello.png", SDL_GetError());
         exit(-1);
     }
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-    SDL_BlitSurface(helloImage, NULL, screenSurface, NULL);
-    SDL_UpdateWindowSurface(window);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, helloTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
     SDL_ShowWindow(window);
 
@@ -41,17 +46,46 @@ int main() {
             if (event.type == SDL_QUIT) {
                 running = 0;
             }
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_UP:
+                    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                    SDL_RenderClear(renderer);
+                    SDL_RenderPresent(renderer);
+                    break;
+
+                case SDLK_DOWN:
+                    SDL_SetRenderDrawColor(renderer, 0xCC, 0xCC, 0xCC, 0xFF);
+                    SDL_RenderClear(renderer);
+                    SDL_RenderPresent(renderer);
+                    break;
+
+                case SDLK_RIGHT:
+                    SDL_RenderClear(renderer);
+                    SDL_RenderCopy(renderer, helloTexture, NULL, NULL);
+                    SDL_RenderPresent(renderer);
+                    break;
+
+                default:
+                    break;
+                }
+            }
         }
 
         SDL_Delay(32);
     }
 
-    SDL_FreeSurface(helloImage);
-    helloImage = NULL;
+    SDL_DestroyTexture(helloTexture);
+    helloTexture = NULL;
+
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
 
     SDL_DestroyWindow(window);
     window = NULL;
 
+    IMG_Quit();
     SDL_Quit();
 
     return 0;
